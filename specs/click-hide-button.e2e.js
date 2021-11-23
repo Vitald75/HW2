9c85def3427a1a129062f40f141a3a94fd9c306c
Vitali Dolgoshei <Vitald75@gmail.com>
2021-11-23 15:26:34 +0300

    HW2_v3

--- specs/click-hide-button.e2e.js
+++ specs/click-hide-button.e2e.js
@@ -1,41 +1,42 @@
 // REMOVE THE BELOW CODE BEFORE START THE EXERCISE
 
 describe("Check web site", function () {
-  it("click on hide button three times", async function () {
+  before("open url and login with ...", async function () {
     await browser.url("https://viktor-silakov.github.io/course-sut");
-    await $$("#login")[0].setValue("walker@jw.com");
-    await $$("#password")[0].setValue("password");
-    await $$("button")[0].click();
-    await browser.pause(10000);
+    await $("#login").setValue("walker@jw.com");
+    await $("#password").setValue("password");
+    await $("button").click();
+    const spinner = await $("#spinner");
+    await spinner.waitForDisplayed({
+      reverse: true,
+      timeoutMsg: "Spinner does not hide",
+      timeout: 15000,
+    });
+  });
 
-    // deleting sticky header
+  it("click on hide button three times", async function () {
     await browser.execute(
       'document.querySelector("header.sticky-top").remove()'
     );
-
-    // The first click on alert button using WebdriverIO 
-    await browser.pause(1000);
-    await $$("//button[text()='alert']")[0].click();
-    await browser.pause(1000);
+    // The first method  click on alert button using WebdriverIO
+    await $("//button[text()='alert']").click();
+    await browser.pause(500);
     await browser.acceptAlert();
-    await browser.pause(1000);
 
-    // The second click on alert button using JS browser context 
-    // with function & arguments 
-    await browser.execute(() => { 
-      document.querySelector(arguments[0]).click()
-    } ,"button.btn-danger"
-    );
-    await browser.pause(1000);
+    // The second method click on alert button using JS browser context
+    // with function & arguments
+    await browser.execute(() => {
+      document.querySelector(arguments[0]).click();
+    }, "button.btn-danger");
+    await browser.pause(500);
     await browser.acceptAlert();
-    await browser.pause(1000);
-    
-    // The third click on alert button using JS browser context 
+
+    // The third method click on alert button using JS browser context
     // with script in argument
-    await browser.execute('document.querySelector("button.btn-danger").click()');
-    await browser.pause(1000);
+    await browser.execute(
+      'document.querySelector("button.btn-danger").click()'
+    );
+    await browser.pause(500);
     await browser.acceptAlert();
-    
-    await browser.pause(1000);
   });
 });
--- specs/login-layout.e2e.js
+++ specs/login-layout.e2e.js
@@ -1,50 +1,43 @@
+// REMOVE THE BELOW CODE BEFORE START THE EXERCISE
+
 describe("Check login", function () {
-   
   function assertBgColor(backgroudColor, text) {
-      const redColor = "rgba(255,0,0,1)";
-      if (backgroudColor == redColor) {
-        throw new Error(`The menu ${text} has wrong background color`);
-      }
-      console.log(text);
+    const redColor = "rgba(255,0,0,1)";
+    if (backgroudColor == redColor) {
+      throw new Error(`The menu ${text} has wrong background color`);
     }
+    console.log(text);
+  }
 
-  it("Check login user", async function () {
-  
+  it("Check login user and check menu background color", async function () {
     await browser.url("https://viktor-silakov.github.io/course-sut");
-    await $$("#login")[0].setValue("walker@jw.com");
-    await $$("#password")[0].setValue("password");
-    await $$('button[onclick="login();"]')[0].click();
-
-    await $$("#spinner")[0].waitForDisplayed({
-      reverse: false,
+    await $("#login").setValue("walker@jw.com");
+    await $("#password").setValue("password");
+    await $('button[onclick="login();"]').click();
+    const spinner = await $("#spinner");
+    await spinner.waitForDisplayed({
+      reverse: true,
       timeoutMsg: "Spinner does not hide",
-      timeout: 5000,
+      timeout: 15000,
     });
-    await browser.pause(12000);
-
-    const logElem = await $$('a[title="walker@jw.com"]')[0];
+    const logElem = await $('a[title="walker@jw.com"]');
     const logName = await logElem.getText();
     const logElemIsDisplayed = await logElem.isDisplayed();
-
     if (!(logName == "John Walker" && logElemIsDisplayed)) {
       throw new Error("User named Jonh Walker did not login into system!!!");
     }
-  });
-    
-  it("Check menu background color", async function () {
-    
     const menuElements = await $$("#first-nav-block>li");
-
     for (const menuElement of menuElements) {
       const textElement = await menuElement.getText();
+      console.log({ textElement });
       await menuElement.moveTo();
-      await browser.pause(1000);
-      //const elementBackgroundStyle = await menuElement.getCSSProperty("background-color");
-      //const elementBackgroundColor = elementBackgroundStyle.value;
-
-      const elementBackgroundColor = (await menuElement.getCSSProperty("background-color")).value;
-
-      assertBgColor(elementBackgroundColor, textElement);          
+      await browser.pause(500);
+      const textBackground = await menuElement.getCSSProperty(
+        "background-color"
+      );
+      const colorBackground = textBackground.value;
+      assertBgColor(colorBackground, textElement);
     }
   });
 });
+//});
